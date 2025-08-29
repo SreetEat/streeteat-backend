6aac4c7786f30e4d4e7422a9df7fd06dc8f24fef
priyanshikoundil <pryanshi28012004@icloud.com>
2025-08-29 12:40:42 +0530

    initial commit - StreetEat backend

---
+++ src/main/java/com/streeteat/streeteat/StreetEatApplication.java
@@ -0,0 +1,13 @@
+package com.streeteat.streeteat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetEatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/streeteat/streeteat/StreetEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streeteat.streeteat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetEatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
